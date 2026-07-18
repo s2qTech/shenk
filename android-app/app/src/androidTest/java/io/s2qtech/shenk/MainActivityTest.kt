20f0c4ff00f18ab49c1aeec04810b020904f597f
s2qTech <s2qTech@users.noreply.github.com>
2026-07-18 17:58:08 +0800

    fix(android): compile native timer instrumentation

--- android-app/app/src/androidTest/java/io/s2qtech/shenk/MainActivityTest.kt
+++ android-app/app/src/androidTest/java/io/s2qtech/shenk/MainActivityTest.kt
@@ -1,7 +1,6 @@
 package io.s2qtech.shenk
 
 import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.assertExists
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onAllNodesWithTag
 import androidx.compose.ui.test.onNodeWithText
